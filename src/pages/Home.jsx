cf141f6c29ca89b012d08526f4a831295338aaf4
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-22 14:21:45 +0600

    project layout and resources created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+           <h1 className="text-center font-semibold text-3xl">Car Doctor</h1> 
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3>this is navbar</h3>
+        </div>
+    );
+};
+
+export default Navbar;
